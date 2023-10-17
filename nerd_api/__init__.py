e1e6fb05bac4076334cc8c41906ccbd2395c7c98
runtimejpp <jpnamausa@gmail.com>
2023-10-17 13:20:29 -0400

    celery up flower down

--- nerd_api/__init__.py
+++ nerd_api/__init__.py
@@ -36,7 +36,41 @@ mailhog        | 2023/10/17 16:53:36 Using in-memory storage
 adding as list = 
 
 ... 
-celery_worker  | PostgreSQL is available
-celery_worker  | [16:53:40] watchfiles v0.18.1 👀  path="/app" target="celery.__main__.main" (function) filter=DefaultFilter...
-mailhog        | 2023/10/17 16:53:36 Using in-memory storage
+celery_worker   | [2023-10-17 16:53:46,194: WARNING/SpawnProcess-1] /usr/local/lib/python3.12/site-packages/celery/worker/consumer/consumer.py:507: CPendingDeprecationWarning: The broker_connection_retry configuration setting will no longer determine
+celery_worker   | whether broker connection retries are made during startup in Celery 6.0 and above.
+celery_worker   | If you wish to retain the existing behavior for retrying connections on startup,
+celery_worker   | you should set broker_connection_retry_on_startup to True.
+celery_worker   |   warnings.warn(
+celery_worker   |
+celery_worker   | [2023-10-17 16:53:46,203: INFO/SpawnProcess-1] mingle: searching for neighbors
+celery_worker   | [2023-10-17 16:53:47,236: INFO/SpawnProcess-1] mingle: all alone
+celery_worker   | [2023-10-17 16:53:47,261: INFO/SpawnProcess-1] celery@174e2895e6ed ready.
+celery_worker   | [16:57:32] 1 change detected
+celery_worker   |
+celery_worker   | worker: Hitting Ctrl+C again will terminate all running tasks!
+celery_worker   |
+celery_worker   | worker: Warm shutdown (MainProcess)
+celery_worker   | /usr/local/lib/python3.12/site-packages/celery/platforms.py:829: SecurityWarning: You're running the worker with superuser privileges: this is
+celery_worker   | absolutely not recommended!
+celery_worker   |
+celery_worker   | Please specify a different user using the --uid option.
+celery_worker   |
+celery_worker   | User information: uid=0 euid=0 gid=0 egid=0
+celery_worker   |
+celery_worker   |   warnings.warn(SecurityWarning(ROOT_DISCOURAGED.format(
+celery_worker   |
+celery_worker   |  -------------- celery@174e2895e6ed v5.3.4 (emerald-rush)
+celery_worker   | --- ***** -----
+celery_worker   | -- ******* ---- Linux-6.4.16-linuxkit-x86_64-with-glibc2.31 2023-10-17 16:57:42
+celery_worker   | - *** --- * ---
+celery_worker   | - ** ---------- [config]
+celery_worker   | - ** ---------- .> app:         nerd_api:0x7fb160a7ed20
+celery_worker   | - ** ---------- .> transport:   redis://redis:6379/0
+celery_worker   | - ** ---------- .> results:     redis://redis:6379/0
+celery_worker   | - *** --- * --- .> concurrency: 4 (prefork)
+celery_worker   | -- ******* ---- .> task events: OFF (enable -E to monitor tasks in this worker)
+celery_worker   | --- ***** -----
+celery_worker   |  -------------- [queues]
+celery_worker   |                 .> celery           exchange=celery(direct) key=celery
+
 """
